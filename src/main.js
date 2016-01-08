8e40f2f320dc71d2ae72d4ecc2cc9591c971c4ed
weifei <weifei@patsnap.com>
2016-01-08 11:19:28 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,4 @@
+'use strict'
+
+
+require('./javascript/test.js')('Gulp');
---
+++ webpack/gulpfile.js
@@ -0,0 +1,117 @@
+'use strict'
+
+// Load some modules which are installed through NPM.
+var gulp = require('gulp');
+// node 中的 path 模块
+var path = require('path');
+// 实现能在浏览器端像 node 后端一样使用模块化
+var browserify = require('browserify');
+// 兼容 gulp 与 node 中的流
+var source = require('vinyl-source-stream');
+// 压缩
+var uglify = require('gulp-uglify');
+//
+var rename = require('gulp-rename');
+// custom utils
+var utils = require('./utils.js');
+
+const DEST_DIR = './dest/';
+const DEST_FILE = 'patsnap.chart.js';
+
+var watchTasks = [];
+var buildTasks = [];
+var allGlob = [];
+
+// 编译检查的状态
+var isOnErrorState = false;
+
+
+/********************************** js file ***********************************/
+
+/**
+ * 打包文件，包括：验证、转换等
+ * @param  {[type]} gulp [description]
+ * @return {[type]}      [description]
+ */
+function getJSPackage (gulp) {
+	utils.logMsg(false, {
+		state: '2',
+		name: 'javascript',
+		operator: '1'
+	});
+
+	return function () {
+
+		return browserify({ debug: false })
+	        //.transform(babelify)
+	        .require('./src/main.js', { entry: true })
+	        .bundle(function(err,bff){
+	            if (bff) {
+	                // 避免一直弹窗
+	                isOnErrorState && utils.popMsg(isOnErrorState = false);
+	            }
+
+	            utils.logMsg(false, {
+	                state: '1',
+	                name: 'javascript',
+					operator: '2'
+	            });
+	        })
+	        .on("error", function (err) {
+	            utils.logMsg(true, err);
+	            this.emit('end');
+	            // 避免一直弹窗
+	            !isOnErrorState && utils.popMsg(isOnErrorState = true, err);
+	        })
+	        .pipe(source(DEST_FILE))
+	        .pipe(gulp.dest(DEST_DIR));
+	};
+}
+/**
+ * 压缩已打包的文件
+ * @param  {[type]} gulp [description]
+ * @return {[type]}      [description]
+ */
+function uglifyJSPackage (gulp) {
+	utils.logMsg(false, {
+		state: '2',
+		name: 'javascript uglify',
+		operator: '1'
+	});
+
+	return function () {
+
+		return gulp.src(path.join(DEST_DIR, DEST_FILE))
+	        .pipe(uglify())
+	        .pipe(rename({ extname: '.min.js' }))
+	        .pipe(gulp.dest(DEST_DIR));
+	};
+}
+// add task
+allGlob.push('src/**/**.js');
+watchTasks.push('javascript');
+gulp.task(watchTasks[watchTasks.length - 1], getJSPackage(gulp));
+buildTasks.push('javascript-b');
+gulp.task(buildTasks[buildTasks.length - 1],
+    [watchTasks[watchTasks.length - 1]],
+    uglifyJSPackage(gulp));
+
+
+/********************************* css file ***********************************/
+
+
+
+
+gulp.task('build', buildTasks);
+gulp.task('watch', function() {
+    return gulp.watch(allGlob, watchTasks);
+});
+module.exports = {
+    watch: function () {
+        gulp.start('watch');
+    },
+    build: function () {
+
+        gulp.start('build');
+    }
+};
